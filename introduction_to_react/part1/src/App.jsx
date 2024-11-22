ff2af06d10f8a42ed82861e5d42728ff3764b707
Stef Ruinard <ruinard@google.com>
2024-11-22 15:47:59 +0000

    1.14*: anecdotes step 3

--- introduction_to_react/part1/src/App.jsx
+++ introduction_to_react/part1/src/App.jsx
@@ -1,68 +1,72 @@
 import { useState } from 'react'
 
-const Button = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <button onClick={props.handleClick}>{props.text}</button>
-    </div>
-  )
-}
-
-const StatisticsLine = ({ text, value }) => (
-  <>
-    <p>{text} {value}</p>
-  </>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
-const Statistics = (props) => {
-
-  {
-    if (props.total === 0) {
-      return <p>No feedback given</p>
-    } else return (
-      <>
-        <h1>Statistics</h1>
-        <div>
-          <StatisticsLine text="good" value={props.good} />
-          <StatisticsLine text="neutral" value={props.neutral} />
-          <StatisticsLine text="bad" value={props.bad} />
-          <StatisticsLine text="all" value={props.total} />
-          <StatisticsLine text="average" value={props.average} />
-          <StatisticsLine text="positive" value={props.positivePercentage} />
-        </div>
-      </>
-    )
-  }
-}
+const QuoteDisplay = (props) => (
+  <div>
+    <h1>{props.title}</h1>
+    <div>
+      <p> {props.quote} </p>
+      <p>has {props.votes} votes</p>
+    </div>
+  </div>
+)
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0
+  })
 
-  // Calculate total feedback
-  const total = good + neutral + bad;
 
-  // Calculate average score
-  const average = total > 0 ? (good - bad) / total : 0;
+  const handleClick = () => {
+    const random = Math.floor(Math.random() * anecdotes.length)
+    console.log(random)
+    setSelected(random)
+  }
 
-  // Calculate percentage of positive feedback
-  const positivePercentage = total > 0 ? (good / total) * 100 : 0;
-
+  const handleVote = () => {
+    setVotes({...votes, [selected]: votes[selected] + 1})
+  }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <div style={{ display: 'flex' }}>
-        <Button handleClick={() => setGood(good + 1)} text="good" />
-        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <QuoteDisplay
+        title="Anecdote of the day"
+        quote={anecdotes[selected]}
+        votes={votes[selected] || 0}
+      />
+      <div>
+      <Button handleClick={handleVote} text="vote"/>
+      <Button handleClick={handleClick} text="next quote"/>
       </div>
-
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positivePercentage={positivePercentage} />
-
+      <QuoteDisplay
+        title="Anecdote with most votes"
+        quote={anecdotes[Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b)]}
+        votes={votes[Object.keys(votes).reduce((a, b) => votes[a] > votes[b] ? a : b)]}
+      />
     </div>
   )
 }
