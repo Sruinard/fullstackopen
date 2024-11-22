bba97949c9d189ccacd680821664f47af1896909
Stef Ruinard <ruinard@google.com>
2024-11-22 15:29:00 +0000

    part 1.11

--- introduction_to_react/part1/src/App.jsx
+++ introduction_to_react/part1/src/App.jsx
@@ -1,62 +1,69 @@
-const Header = (props) => {
-  return (
-      <h1>{props.course}</h1>
-  )
-}
+import { useState } from 'react'
 
-const Part = (props) => {
+const Button = (props) => {
   console.log(props)
   return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-
-const Content = (props) => {
-  return (
     <div>
-      {
-        props.parts.map((part, index) => <Part key={index} part={part} />)
-      }
+      <button onClick={props.handleClick}>{props.text}</button>
     </div>
   )
 }
 
-const Total = (props) => {
+const StatisticsLine = ({ text, value }) => (
+  <>
+    <p>{text} {value}</p>
+  </>
+)
 
-  const total = props.parts.reduce((acc, part) => acc + part.exercises, 0)
-  return <p>Number of exercises {total}</p>
+const Statistics = (props) => {
+
+  {
+    if (props.total === 0) {
+      return <p>No feedback given</p>
+    } else return (
+      <>
+        <h1>Statistics</h1>
+        <div>
+          <StatisticsLine text="good" value={props.good} />
+          <StatisticsLine text="neutral" value={props.neutral} />
+          <StatisticsLine text="bad" value={props.bad} />
+          <StatisticsLine text="all" value={props.total} />
+          <StatisticsLine text="average" value={props.average} />
+          <StatisticsLine text="positive" value={props.positivePercentage} />
+        </div>
+      </>
+    )
+  }
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
+  // Calculate total feedback
+  const total = good + neutral + bad;
+
+  // Calculate average score
+  const average = total > 0 ? (good - bad) / total : 0;
+
+  // Calculate percentage of positive feedback
+  const positivePercentage = total > 0 ? (good / total) * 100 : 0;
 
 
   return (
-    <>
-    <Header course={course.name} />
-    <Content parts={course.parts}/>
-    <Total parts={course.parts}/>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <div style={{ display: 'flex' }}>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      </div>
+
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positivePercentage={positivePercentage} />
+
+    </div>
   )
 }
 
