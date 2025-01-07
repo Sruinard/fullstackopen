fa017119d73cb01b7371f052719d99da79c2cfa8
Stef Ruinard <ruinard@google.com>
2025-01-07 07:29:46 +0000

    up until 4.19

--- backend_testing/blog/app.js
+++ backend_testing/blog/app.js
@@ -7,6 +7,8 @@ const config = require('./utils/config')
 const blogRouter = require('./controllers/blogposts')
 const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 mongoose.connect(config.MONGODB_URI)    
     .then(() => {
@@ -20,6 +22,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
--- backend_testing/blog/controllers/blogposts.js
+++ backend_testing/blog/controllers/blogposts.js
@@ -1,18 +1,46 @@
+const jwt = require('jsonwebtoken')
 const Router = require('express').Router()
 const Blog = require('../models/blogpost')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 
 Router.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 })
   response.status(200).json(blogs)
 })
 
 Router.post('/', async (request, response, next) => {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const {title, author, url, likes} = request.body
+  const user = await User.findById(decodedToken.id)
 
   try {
-    const blog = new Blog(request.body)
+    const blog = new Blog({
+      title,
+      author,
+      url,
+      likes,
+      user: user.id
+    })
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
+
   } catch(exception) {
     console.log('failed to add new blog...')
     next(exception)
