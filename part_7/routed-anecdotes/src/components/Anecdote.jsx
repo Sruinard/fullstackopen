a1cf0ecae0784a28493d5515e4db5cfeb42840ad
Stef Ruinard <ruinard@google.com>
2025-02-07 16:20:07 +0000

    up to 7.3

---
+++ part_7/routed-anecdotes/src/components/Anecdote.jsx
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom'
+
+export const Anecdote = ({ anecdotes }) => {
+    const { id } = useParams()
+    const anecdote = anecdotes.find(a => a.id === Number(id))
+    return (
+        <div>
+            <h2>{anecdote.content}</h2>
+            <p>has {anecdote.votes} votes</p>
+            <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+        </div>
+    )
+}
